5144fbb7f06758a4030eaccc5c9f4f0fe0adc190
Aml-Abbas <aml.abbas@hotmail.com>
2020-11-11 20:48:43 +0100

    Initialize commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,47 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BuyerProfileComponent} from "./components/buyer-profile/buyer-profile.component";
+import {BuyerSignupComponent} from "./components/buyer-signup/buyer-signup.component";
+import {BuyerStartComponent} from "./components/buyer-start/buyer-start.component";
+import {BuyerLoginComponent} from "./components/buyer-login/buyer-login.component";
+import {BuyerProfilEditorComponent} from "./components/buyer-profil-editor/buyer-profil-editor.component";
+import {SellerProfileComponent} from "./components/seller-profile/seller-profile.component";
+import {SellerProfilEditorComponent} from "./components/seller-profil-editor/seller-profil-editor.component";
+import {SellerDetailsComponent} from "./components/seller-details/seller-details.component";
+import {SellerMyProductsComponent} from "./components/seller-my-products/seller-my-products.component";
+import {SellerOrderDetailsComponent} from "./components/seller-order-details/seller-order-details.component";
+import {SellerProductsComponent} from "./components/seller-products/seller-products.component";
+import {SellersListComponent} from "./components/sellers-list/sellers-list.component";
+import {SellerWeekOrdersComponent} from "./components/seller-week-orders/seller-week-orders.component";
+import {ConfirmationComponent} from "./components/confirmation/confirmation.component";
+import {ConfirmationLoginComponent} from "./components/confirmation-login/confirmation-login.component";
+import {ConfirmationNoLoginComponent} from "./components/confirmation-no-login/confirmation-no-login.component";
+import {ShoppingCartComponent} from "./components/shopping-cart/shopping-cart.component";
+import {LandingComponent} from "./components/landing/landing.component";
+import {ProductEditorComponent} from "./components/product-editor/product-editor.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'buyer-login', component: BuyerLoginComponent},
+  {path: 'buyer-profil-editor', component: BuyerProfilEditorComponent},
+  {path: 'buyer-profil', component: BuyerProfileComponent},
+  {path: 'buyer-signup', component: BuyerSignupComponent},
+  {path: 'buyer-start', component: BuyerStartComponent},
+  {path: 'confirmation', component: ConfirmationComponent},
+  {path: 'confirmation-login', component: ConfirmationLoginComponent},
+  {path: 'confirmation-no-login', component: ConfirmationNoLoginComponent},
+  {path: 'ladning', component: LandingComponent},
+  {path: 'product-editor', component: ProductEditorComponent},
+  {path: 'seller-details', component: SellerDetailsComponent},
+  {path: 'seller-my-products', component: SellerMyProductsComponent},
+  {path: 'seller-order-details', component: SellerOrderDetailsComponent},
+  {path: 'seller-products', component: SellerProductsComponent},
+  {path: 'seller-profil-editor', component: SellerProfilEditorComponent},
+  {path: 'seller-week-orders', component: SellerWeekOrdersComponent},
+  {path: 'seller-profil', component: SellerProfileComponent},
+  {path: 'sellers-list', component: SellersListComponent},
+  {path: 'shopping-cart', component: ShoppingCartComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { BuyerStartComponent } from './components/buyer-start/buyer-start.component';
+import { BuyerLoginComponent } from './components/buyer-login/buyer-login.component';
+import { SellersListComponent } from './components/sellers-list/sellers-list.component';
+import { SellerDetailsComponent } from './components/seller-details/seller-details.component';
+import { SellerProductsComponent } from './components/seller-products/seller-products.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ConfirmationLoginComponent } from './components/confirmation-login/confirmation-login.component';
+import { ConfirmationNoLoginComponent } from './components/confirmation-no-login/confirmation-no-login.component';
+import { ConfirmationComponent } from './components/confirmation/confirmation.component';
+import { BuyerSignupComponent } from './components/buyer-signup/buyer-signup.component';
+import { SellerMyProductsComponent } from './components/seller-my-products/seller-my-products.component';
+import { ProductEditorComponent } from './components/product-editor/product-editor.component';
+import { SellerWeekOrdersComponent } from './components/seller-week-orders/seller-week-orders.component';
+import { SellerOrderDetailsComponent } from './components/seller-order-details/seller-order-details.component';
+import { SellerProfilEditorComponent } from './components/seller-profil-editor/seller-profil-editor.component';
+import { SellerProfileComponent } from './components/seller-profile/seller-profile.component';
+import { BuyerProfileComponent } from './components/buyer-profile/buyer-profile.component';
+import { BuyerProfilEditorComponent } from './components/buyer-profil-editor/buyer-profil-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    BuyerStartComponent,
+    BuyerLoginComponent,
+    SellersListComponent,
+    SellerDetailsComponent,
+    SellerProductsComponent,
+    ShoppingCartComponent,
+    ConfirmationLoginComponent,
+    ConfirmationNoLoginComponent,
+    ConfirmationComponent,
+    BuyerSignupComponent,
+    SellerMyProductsComponent,
+    ProductEditorComponent,
+    SellerWeekOrdersComponent,
+    SellerOrderDetailsComponent,
+    SellerProfilEditorComponent,
+    SellerProfileComponent,
+    BuyerProfileComponent,
+    BuyerProfilEditorComponent
   ],
   imports: [
     BrowserModule,
