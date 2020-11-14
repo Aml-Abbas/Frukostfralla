56ff1a90e7494dce59caf85c72dcdcbed047ffad
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-11-14 20:13:38 +0100

    Many changes, many changes indeed

--- src/app/components/seller-profile-editor/seller-profile-editor.component.ts
+++ src/app/components/seller-profile-editor/seller-profile-editor.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
 
 @Component({
   selector: 'app-seller-profile-editor',
   templateUrl: './seller-profile-editor.component.html',
-  styleUrls: ['./seller-profile-editor.component.scss']
+  styleUrls: ['./seller-profile-editor.component.scss'],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
+
 })
 export class SellerProfileEditorComponent implements OnInit {
+  title = 'Redigera profil'
 
-  constructor() { }
+  location: Location
+  enteredPassword = '';
+  hide = true;
+
+  constructor(location: Location) {
+    this.location = location;
+  }
 
   ngOnInit(): void {
   }
+
+  public navigateBack(): void {
+    this.location.back();
+  }
+
 
 }
