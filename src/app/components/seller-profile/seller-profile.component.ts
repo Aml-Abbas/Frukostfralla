447b2fec1afdbabec8955e7b0889271e13215f36
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-11-14 18:17:25 +0100

    coded seller-profile.component.html and seller-profile.component.scss

--- src/app/components/seller-profile/seller-profile.component.ts
+++ src/app/components/seller-profile/seller-profile.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-seller-profile',
   templateUrl: './seller-profile.component.html',
   styleUrls: ['./seller-profile.component.scss']
 })
-export class SellerProfileComponent implements OnInit {
 
-  constructor() { }
+export class SellerProfileComponent implements OnInit {
+  title = 'Min profil';
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
