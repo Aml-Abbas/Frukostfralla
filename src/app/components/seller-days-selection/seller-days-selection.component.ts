6d2cbf230f9770e37e5bf15a7aa1253acac2f97d
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-11-16 17:24:42 +0100

    Coded Seller days selection page

--- src/app/components/seller-days-selection/seller-days-selection.component.ts
+++ src/app/components/seller-days-selection/seller-days-selection.component.ts
@@ -1,15 +1,54 @@
 import { Component, OnInit } from '@angular/core';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {Router} from '@angular/router';
+import {MatListOption} from '@angular/material/list';
 
 @Component({
   selector: 'app-seller-days-selection',
   templateUrl: './seller-days-selection.component.html',
-  styleUrls: ['./seller-days-selection.component.scss']
+  styleUrls: ['./seller-days-selection.component.scss'],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
 })
 export class SellerDaysSelectionComponent implements OnInit {
 
-  constructor() { }
+  title = 'Välj veckodagar';
+  location: Location;
+  router: Router;
+
+  weekDays: string[] = ['Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag', 'Söndag']
+  selectedWeekDays: string[];
+
+  constructor(location: Location, router: Router) {
+    this.location = location;
+    this.router = router;
+
+    this.selectedWeekDays = [];
+  }
 
   ngOnInit(): void {
+    /**
+     *  TODO: Make a get call to the API to get the user's week days and save them in the array weekDays
+     *  this.weekDays = days from the backend
+     */
+  }
+
+  public navigateBack(): void {
+    this.location.back();
+  }
+
+  onGroupsChange(selectedDays: string[]): void {
+    console.log(selectedDays);
+    this.selectedWeekDays = selectedDays;
+  }
+
+  onSave(): void {
+    // TODO: Make a post call to the API to save the data
+    this.router.navigate(['/seller-profile'], {replaceUrl: true});
+
+  }
+
+  onCancel(): void {
+    this.router.navigate(['/seller-profile'], {replaceUrl: true});
   }
 
 }
