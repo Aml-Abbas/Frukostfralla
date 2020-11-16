19b388e799b9ce8e0807ea99307215446fc2a9cb
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-11-17 00:13:34 +0100

    created seller-order-details.component.html toolbar seller-order-details.component.ts variables

--- src/app/components/seller-order-details/seller-order-details.component.ts
+++ src/app/components/seller-order-details/seller-order-details.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {OrderDetailsService} from '../../../services/order-details.service';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {Router} from '@angular/router';
+import {SellerOrderDetailsProduct} from '../../model/SellerOrderDetailsProduct';
+
 
 @Component({
   selector: 'app-seller-order-details',
@@ -7,14 +11,33 @@ import {OrderDetailsService} from '../../../services/order-details.service';
   styleUrls: ['./seller-order-details.component.scss']
 })
 export class SellerOrderDetailsComponent implements OnInit {
+  location: Location;
+  router: Router;
 
   orderId: string;
+  title = '';
 
-  constructor(private orderDetails: OrderDetailsService) { }
+  buyer: string;
+  address: string;
+  mobileNumber: string;
+  total: number;
+  products: SellerOrderDetailsProduct[] = [];
+
+  constructor(location: Location, router: Router, private orderDetails: OrderDetailsService) { }
 
   ngOnInit(): void {
     this.orderDetails.currentOrderId$.subscribe(orderId => this.orderId = orderId);
     console.log("order id = " + this.orderId);
+    this.title = 'Beställning #' + this.orderId;
   }
+
+  navigateBack(): void {
+    this.location.back();
+  }
+
+  fetchOrderDetails(orderId: string) {
+
+  }
+
 
 }
