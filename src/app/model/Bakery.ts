5da95fbd2c348082b828f1969175aa936cf924b9
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-11-17 11:09:35 +0100

    Created Bakery.ts class

---
+++ src/app/model/Bakery.ts
@@ -0,0 +1,16 @@
+export class Bakery {
+  name: string;
+  city: string;
+  area: string;
+  street: string;
+  county: string;
+
+
+  constructor(name: string, city: string, area: string, street: string, county: string) {
+    this.name = name;
+    this.city = city;
+    this.area = area;
+    this.street = street;
+    this.county = county;
+  }
+}
