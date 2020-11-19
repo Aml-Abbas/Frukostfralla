d0432fa7ae18c0009233fbf91480373c44507cad
Aml-Abbas <aml.abbas@hotmail.com>
2020-11-19 11:31:20 +0100

    fix-CSS- Toolbar

--- src/app/components/seller-details/seller-details.component.ts
+++ src/app/components/seller-details/seller-details.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-seller-details',
   templateUrl: './seller-details.component.html',
-  styleUrls: ['./seller-details.component.scss']
+  styleUrls: ['./seller-details.component.scss'],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
+
 })
 export class SellerDetailsComponent implements OnInit {
+  title = 'Säljarens namn';
 
   constructor() { }
 
