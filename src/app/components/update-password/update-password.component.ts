ab142c2f800becc811898c2dcdfe12f4b0154f7b
Abdusalam Yabrak <abdusalam.yab1996@gmail.com>
2020-11-22 22:04:03 +0100

    Basic password updating logic with fake data from password.service.ts

--- src/app/components/update-password/update-password.component.ts
+++ src/app/components/update-password/update-password.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
+import {PasswordService} from '../../../services/password.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-update-password',
@@ -9,15 +11,17 @@ import {Location} from '@angular/common';
 export class UpdatePasswordComponent implements OnInit {
   title = 'Uppdatera ditt lösenord';
 
-  currentPassword = '';
-  enteredPassword = '';
-  enteredPassword2 = '';
+  enteredCurrentPassword = '';
+  enteredNewPassword = '';
+  enteredNewPassword2 = '';
 
   hideCurrent = true;
 
   hide = true;
 
-  constructor(private location: Location) {
+  constructor(private location: Location,
+              private passwordService: PasswordService,
+              private snackBar: MatSnackBar) {
   }
 
   ngOnInit(): void {
@@ -25,5 +29,28 @@ export class UpdatePasswordComponent implements OnInit {
 
   navigateBack() {
     this.location.back();
+  }
+
+  confirm() {
+    if (this.hashPassword(this.enteredCurrentPassword.trim())
+      !== this.passwordService.getUsersCurrentPassword('')) {
+      this.snackBar.open('Fel nuvarande lösenord!', 'Ok', {
+        duration: 2000
+      });
+    } else if (this.enteredNewPassword !== this.enteredNewPassword2) {
+      this.snackBar.open('Lösenorden matchar inte!', 'Ok', {
+        duration: 2000
+      });
+    } else {
+      this.snackBar.open('Lösenord uppdaterat', 'Ok', {
+        duration: 2000
+      });
+      this.passwordService.saveNewPassword(this.enteredNewPassword);
+      this.location.back();
+    }
+  }
+
+  hashPassword(pass: string) {
+    return pass;
   }
 }
