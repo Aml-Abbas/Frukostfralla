5177ebe65a433969a218f66035312fecb4254685
Aml-Abbas <aml.abbas@hotmail.com>
2020-11-25 21:51:00 +0100

    fixed map-tollbar

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {Location, LocationStrategy, PathLocationStrategy} from '@angular/common';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
+  providers: [Location, {provide: LocationStrategy, useClass: PathLocationStrategy}]
+
 })
 export class MapComponent implements OnInit {
+  router: Router;
 
-  constructor() { }
+  title = 'Säljare i ditt område';
+  location: Location;
+
+
+  constructor(router: Router, location: Location) {
+    this.router = router;
+    this.location = location;
+  }
 
   ngOnInit(): void {
   }
 
+
+  public navigateBack(): void {
+    this.location.back();
+  }
 }
